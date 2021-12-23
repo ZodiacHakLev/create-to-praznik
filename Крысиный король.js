5762dc47f0382c131c176e0e52f5d44ae6ca0a6f
ZodiacHakLev <96593592+ZodiacHakLev@users.noreply.github.com>
2021-12-23 20:58:20 +0200

    Add files via upload

---
+++ Крысиный король.js
@@ -0,0 +1,13 @@
+alert("Страница временно не доступна.")
+
+window.onscroll = function showHeader() {
+
+    var header = document.querySelector(".header");
+    if(window.pageYOffset > 200){
+        header.classList.add("header_fixed");
+    } else{
+        header.classList.remove("header_fixed");
+    }
+
+}
+
